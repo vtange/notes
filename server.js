3aef1b67d714c26aa13a91416d0b6bea4eecc761
Victor Tang <victortang1992@gmail.com>
2016-04-15 18:42:26 -0700

    working scraper for one repo

---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/scrape', function(req, res){
+
+    var repos = 'https://api.github.com/users/vtange/repos';
+
+	request(repos, function(error, response, html){
+		console.log(html);
+		console.log();
+		//response.forEach(function(repo){
+		//	console.log('https://raw.githubusercontent.com/vtange/'+repo.name+'/master/README.md')
+		//});
+    });
+	
+	
+	
+	
+    var url = 'https://raw.githubusercontent.com/vtange/TreeOfSaviorMapdex/master/README.md';
+
+    request(url, function(error, response, html){
+		
+        if(!error){
+            var $ = cheerio.load(html);
+            var json = { text : html };
+
+			// To write to the system we will use the built in 'fs' library.
+			// In this example we will pass 3 parameters to the writeFile function
+			// Parameter 1 :  output.json - this is what the created filename will be called
+			// Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
+			// Parameter 3 :  callback function - a callback function to let us know the status of our function
+			fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+
+				console.log('File successfully written! - Check your project directory for the output.json file');
+
+			});
+
+			// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+			res.send('Check your console!');
+        }//end if(!error)
+
+    });//end request
+	
+});
+
+app.listen('8081');
+
+console.log('Magic happens on port 8081');
+
+exports = module.exports = app;
